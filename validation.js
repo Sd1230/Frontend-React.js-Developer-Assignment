beb2b844a803fff5d9b087fd387a7f8c792a6226
saurabh singh <112756443+Sd1230@users.noreply.github.com>
2023-12-02 19:36:11 +0530

    Create validation.js

---
+++ validation.js
@@ -0,0 +1,9 @@
+export const validateWalletAddress = (address) => {
+  // Add wallet address validation logic
+  return true; // Placeholder
+};
+
+export const validateAmount = (amount) => {
+  // Add amount validation logic
+  return true; // Placeholder
+};
