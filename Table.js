ebbda60a9599145f783367d17370b7b79e6e8178
saurabh singh <112756443+Sd1230@users.noreply.github.com>
2023-12-02 19:37:21 +0530

    Create Table.js

---
+++ Table.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Table({ posts }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Body</th>
+        </tr>
+      </thead>
+      <tbody>
+        {posts.map((post) => (
+          <tr key={post.id}>
+            <td>{post.id}</td>
+            <td>{post.title}</td>
+            <td>{post.body}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
